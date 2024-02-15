4f5e871241081e66463a08619954129fb9979645
sen-et <sen-et@users.noreply.github.com>
2024-02-14 18:19:50 -0800

    Complete 01_helloWorld and 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let strReturn = '';
+    for (let i = 1; i <= num; i++) {
+        strReturn += string;
+    }
+    if (num < 0) {
+        return 'ERROR';
+    } else {
+        return strReturn;
+    }
 };
 
 // Do not edit below this line
