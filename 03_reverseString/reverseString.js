37abbd2039e9beccf2d8c3b647cb5603582ed00f
sen-et <sen-et@users.noreply.github.com>
2024-02-16 20:17:41 -0800

    Complete 03_reverseString and 04_removeFromArray

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringSplit = string.split('');
+    let stringReverse = stringSplit.reverse();
+    let stringJoin = stringReverse.join('');
+    return stringJoin;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...valRemove) {
+    let newArray = [];
+    arr.forEach((i) => {
+        if (!valRemove.includes(i)) {
+            newArray.push(i);
+        }
+    });
+    return newArray;
 };
 
 // Do not edit below this line
