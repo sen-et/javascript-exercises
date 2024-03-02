9747f5ab1b9feb739dc56e10209f4ab07b7851ff
sen-et <sen-et@users.noreply.github.com>
2024-03-01 18:03:59 -0800

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if (num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    } else if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return 'ERROR';
+    }
+    if (num1 < num2) {
+        for (let i = num1; i <= num2; i++) {
+            sum += i;
+        }
+    } else if (num1 > num2) {
+        for (let i = num2; i <= num1; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 };
+
+sumAll(1, 10);
 
 // Do not edit below this line
 module.exports = sumAll;
